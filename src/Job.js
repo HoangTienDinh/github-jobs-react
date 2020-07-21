a27460c9ffa73cc20f7c613feebfabd8a335e6ba
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-21 14:24:03 -0700

    npm react-markdown installed

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Badge } from "react-bootstrap";
 
 export default function Job({ job }) {
   return (
@@ -10,9 +10,13 @@ export default function Job({ job }) {
           <Card.Title>
             {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
           </Card.Title>
+          <Card.Subtitle className='text-muted mb-2'>
+              {new Date(job.created_at).toLocaleDateString()}
+          </Card.Subtitle>
+          <Badge variant='secondary' className='mr-2'>{job.type}</Badge>
+          <Badge variant='secondary'>{job.location}</Badge>
         </div>
       </Card.Body>
-      {job.title}
     </Card>
   );
 }
